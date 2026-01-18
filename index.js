286fa09bbe0dbf561170f21dc489910604e91760
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-18 12:00:29 +0600

    created signup api with jwt

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Apnar Next.js app er URL
+    credentials: true, // Eti must! Cookie transaction er jonno dorkar
+  }),
+);
+
+const uri = process.env.MONGODB_URI;
+
+// jwt token generate
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+};
+app.get("/", (req, res) => {
+  res.send("thread & co. is running...");
+});
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("thread");
+    const usersCollection = db.collection("users");
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      if (!user) {
+        return res.status(400).send({ message: "User data is required" });
+      }
+
+      const email = user.email;
+      const isExisting = await usersCollection.findOne({ email });
+      if (isExisting) {
+        return res.status(400).send({ message: "User already exist" });
+      }
+
+      const saltRounds = 10;
+      const plainPassword = user.password;
+      const hashedAdminPassword = await bcrypt.hash(plainPassword, saltRounds);
+      const userToDB = {
+        name: user.name,
+        email: user.email,
+        password: hashedAdminPassword,
+        photoURL: user.photoURL,
+        role: "user",
+        createdAt: new Date(),
+      };
+      const result = await usersCollection.insertOne(userToDB);
+      const newUser = {
+        id: result.insertedId,
+        name: user.name,
+        email: user.email,
+        role: "user",
+      };
+
+      const token = generateToken(newUser);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict",
+          maxAge: 24 * 60 * 60 * 1000,
+        })
+        .send({
+          success: true,
+          message: "Signup successful",
+          user: newUser,
+        });
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`port is listening on ${port}`);
+});

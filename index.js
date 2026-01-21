a396939540b0fc26898036553bfac8e18f65af18
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-21 23:14:34 +0600

    remove password field from login response

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ app.use(
   }),
 );
 
-const uri = process.env.MONGODB_URI;
-
 // jwt token generate
 const generateToken = (user) => {
   return jwt.sign(
@@ -31,7 +29,6 @@ const generateToken = (user) => {
     { expiresIn: "1d" },
   );
 };
-console.log(process.env.SITE_DOMAIN);
 const verifyToken = (req, res, next) => {
   const token = req.cookies?.token;
 
@@ -51,142 +48,141 @@ const verifyToken = (req, res, next) => {
 app.get("/", (req, res) => {
   res.send("thread & co. is running...");
 });
-const client = new MongoClient(uri, {
+
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
 });
-async function run() {
+const db = client.db("thread");
+const usersCollection = db.collection("users");
+
+app.post("/api/register", async (req, res) => {
+  const user = req.body;
+  if (!user) {
+    return res.status(400).send({ message: "User data is required" });
+  }
+
+  const email = user.email;
+  const isExisting = await usersCollection.findOne({ email });
+  if (isExisting) {
+    return res.status(400).send({ message: "User already exist" });
+  }
+
+  const saltRounds = 10;
+  const plainPassword = user.password;
+  const hashedAdminPassword = await bcrypt.hash(plainPassword, saltRounds);
+  const userToDB = {
+    name: user.name,
+    email: user.email,
+    password: hashedAdminPassword,
+    photoURL: user.photoURL,
+    role: "user",
+    createdAt: new Date(),
+  };
+  const result = await usersCollection.insertOne(userToDB);
+  const newUser = {
+    id: result.insertedId,
+    name: user.name,
+    email: user.email,
+    role: "user",
+  };
+
+  const token = generateToken(newUser);
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000,
+    })
+    .send({
+      success: true,
+      message: "Signup successful",
+      user: newUser,
+    });
+});
+// user login api
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.send("email & pass are required");
+  }
+  const user = await usersCollection.findOne({ email });
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.send("password is incorrect!");
+  }
+  const token = generateToken({
+    id: user._id,
+    email: user.email,
+    role: user.role,
+  });
+  const userWithoutPassword = { ...user };
+  delete userWithoutPassword.password;
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 24 * 60 * 60 * 1000,
+    })
+    .send({
+      success: true,
+      message: "login successful",
+      user: userWithoutPassword,
+    });
+});
+app.get("/api/me", verifyToken, async (req, res) => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    const email = req.user.email;
+    const query = { email: email };
+
+    const user = await usersCollection.findOne(query, {
+      projection: { password: 0 },
+    });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+// logout api
+app.post("/api/logout", async (req, res) => {
+  res
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .send({
+      success: true,
+      message: "Logout successful",
+    });
+});
+app.get("/api/users", async (req, res) => {
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+});
+
+async function connectDB() {
+  try {
     await client.connect();
-
-    const db = client.db("thread");
-    const usersCollection = db.collection("users");
-
-    app.post("/register", async (req, res) => {
-      const user = req.body;
-      if (!user) {
-        return res.status(400).send({ message: "User data is required" });
-      }
-
-      const email = user.email;
-      const isExisting = await usersCollection.findOne({ email });
-      if (isExisting) {
-        return res.status(400).send({ message: "User already exist" });
-      }
-
-      const saltRounds = 10;
-      const plainPassword = user.password;
-      const hashedAdminPassword = await bcrypt.hash(plainPassword, saltRounds);
-      const userToDB = {
-        name: user.name,
-        email: user.email,
-        password: hashedAdminPassword,
-        photoURL: user.photoURL,
-        role: "user",
-        createdAt: new Date(),
-      };
-      const result = await usersCollection.insertOne(userToDB);
-      const newUser = {
-        id: result.insertedId,
-        name: user.name,
-        email: user.email,
-        role: "user",
-      };
-
-      const token = generateToken(newUser);
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-          maxAge: 24 * 60 * 60 * 1000,
-        })
-        .send({
-          success: true,
-          message: "Signup successful",
-          user: newUser,
-        });
-    });
-    // user login api
-    app.post("/login", async (req, res) => {
-      const { email, password } = req.body;
-      if (!email || !password) {
-        return res.send("email & pass are required");
-      }
-      const user = await usersCollection.findOne({ email });
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return res.send("password is incorrect!");
-      }
-      const token = generateToken({
-        id: user._id,
-        email: user.email,
-        role: user.role,
-      });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-          maxAge: 24 * 60 * 60 * 1000,
-        })
-        .send({
-          success: true,
-          message: "login successful",
-          user,
-        });
-    });
-    app.get("/me", verifyToken, async (req, res) => {
-      try {
-        const email = req.user.email;
-        const query = { email: email };
-
-        const user = await usersCollection.findOne(query, {
-          projection: { password: 0 },
-        });
-
-        if (!user) {
-          return res.status(404).send({ message: "User not found" });
-        }
-
-        res.send(user);
-      } catch (error) {
-        res.status(500).send({ message: "Internal server error" });
-      }
-    });
-    // logout api
-    app.post("/logout", async (req, res) => {
-      res
-        .clearCookie("token", {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-        })
-        .send({
-          success: true,
-          message: "Logout successful",
-        });
-    });
-    app.get("/users", async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!",
-    // );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log("Database connected!");
+  } catch (err) {
+    console.error(err);
   }
 }
-run().catch(console.dir);
+connectDB();
 
-app.listen(port, () => {
-  console.log(`port is listening on ${port}`);
-});
+module.exports = app;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3001, () => console.log("Server running on 3001"));
+}
